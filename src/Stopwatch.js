9d5083c92b636e4fd931da17c07c9c8a693a796b
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-08-27 14:09:52 -0500

    created methods for each button

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,15 +1,43 @@
-import React, { Component } from 'react';
-import "./Stopwatch.css"
+import React, { Component } from "react";
+import "./Stopwatch.css";
 
 class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0.0,
+      pause: true,
+    };
+  }
+
+  handleReset(event) {
+    console.log(this.state.time, this.state.pause);
+    this.setState({
+      time: 0.0,
+      pause: true,
+    });
+  }
+
+  handleStart(event) {}
+
+  handlePause(event) {
+    let pause = !this.state.pause;
+    console.log(this.state.time, this.state.pause);
+    if (pause) {
+      this.setState({ pause });
+    } else {
+      this.handleStart();
+    }
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={(event) => this.handleReset(event)}>Reset</button>
+          <button onClick={(event) => this.handleStart(event)}>Start</button>
+          <button onClick={(event) => this.handlePause(event)}>Pause</button>
         </div>
       </div>
     );
